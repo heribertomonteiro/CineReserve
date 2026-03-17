75be749a5411cb3994dd2b32c33e41151b9f25a3
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2026-03-17 15:30:02 -0300

    feat: adição de testes de API para listagem de filmes e recuperação de detalhes, e restição de criação de filmes

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from movies.models import Movie
+
+
+class MovieApiTests(APITestCase):
+	def setUp(self):
+		self.movie1 = Movie.objects.create(
+			title="Movie 1",
+			description="Desc 1",
+			duration=100,
+			release_date="2026-01-01",
+		)
+		self.movie2 = Movie.objects.create(
+			title="Movie 2",
+			description="Desc 2",
+			duration=120,
+			release_date="2026-01-02",
+		)
+
+
+	def test_list_movies_is_paginated(self):
+		url = reverse("movie-list")
+		response = self.client.get(url)
+
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertIn("count", response.data)
+		self.assertIn("results", response.data)
+		self.assertGreaterEqual(response.data["count"], 2)
+
+	def test_movie_detail_returns_200(self):
+		url = reverse("movie-detail", kwargs={"pk": self.movie1.id})
+		response = self.client.get(url)
+
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data["id"], self.movie1.id)
+
+	def test_movie_create_not_allowed(self):
+		url = reverse("movie-list")
+		payload = {
+			"title": "New Movie",
+			"description": "Desc",
+			"duration": 90,
+			"release_date": "2026-02-10",
+		}
+
+		response = self.client.post(url, payload, format="json")
+
+		self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+
 from .models import Movie
 from .serializers.movie_serializer import MovieSerializer
 
 
-class MovieViewSet(viewsets.ModelViewSet):
-
-    queryset = Movie.objects.all()
-
+class MovieViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Movie.objects.all().order_by("id")
     serializer_class = MovieSerializer
-   # permission_classes = [IsAuthenticated]
